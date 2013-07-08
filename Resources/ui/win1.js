8faa1c698c3f796da69d51517042fdd2a45fa87a
Jonatan Rodríguez <jrperdomoz@gmail.com>
2013-07-08 17:30:41 +0100

    Actualizo las medidas de los botones y el textField

--- Resources/ui/win1.js
+++ Resources/ui/win1.js
@@ -5,28 +5,28 @@ function Win1(){
 	    dataBase = require('ui/DataBase'),
 		buttonCreate = Ti.UI.createButton({
 			title: 'Create',
-			height: 30,
-			width: 100,
-			top: 0
+			height: 60,
+			width: 120,
+			top: 10
 		}),
 		textField = Ti.UI.createTextField({
 			borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
 			width: 200,
-			top: 35
+			top: 54
 		}),
 		buttonInsert = Ti.UI.createButton({
 			title: 'Insert',
-			width: 60,
-			height: 30,
+			width: 120,
+			height: 60,
 			left: 50,
-			top: 70
+			top: 124
 		}),
 		buttonIDelete = Ti.UI.createButton({
 			title: 'delete',
-			width: 60,
-			height: 30,
+			width: 120,
+			height: 60,
 			right: 50,
-			top: 70
+			top: 124
 		});
 	
 	win.add(buttonCreate);
